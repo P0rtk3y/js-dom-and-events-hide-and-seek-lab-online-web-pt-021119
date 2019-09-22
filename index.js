bd2599eb5441d024de07b94c95fc103927f9072a
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-09-22 23:43:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,14 +6,10 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
-function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for ( let i = 0, l = rankedLists.length; i < l; i++ ) {
-    let children = rankedLists[i].children
-
-    for ( let j = 0, k = children.length; j < k; j++ ) {
-      children[j].innerHTML = parseInt(children[j].innerHTML ) + n
-    }
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i in lis) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
+  return lis;
 }
